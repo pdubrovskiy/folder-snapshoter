c161e3c1d1dcce2db274000c15f6a0e7466e7620
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2023-07-17 23:53:20 +0300

    feat: add navigation section

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
+use std::env;
+
 mod user_interface;
 mod commands;
+mod common;
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut path = common::get_initial_path(&args[0]);
+    
     user_interface::greeting();
 
     loop {
         user_interface::print_menu();
-        commands::run_command(user_interface::get_input());
+        commands::run_command(common::get_input(), &mut path);
     }
 }
-
-
-
-
-
