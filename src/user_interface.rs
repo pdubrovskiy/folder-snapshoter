d3294e16094289d8e7d56f33aada6d386561aa92
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2023-07-27 10:32:03 +0300

    refactor: change menu point

--- src/user_interface.rs
+++ src/user_interface.rs
@@ -6,6 +6,6 @@ pub fn print_menu() {
     println!("|________MENU________|");
     println!("1. navigation");
     println!("2. make snapshot");
-    println!("3. snapshot gallery");
+    println!("3. snapshot gallery & snapshot comparison");
     println!("4. exit");
 }
